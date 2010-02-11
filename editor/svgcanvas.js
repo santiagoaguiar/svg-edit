santiagoaguiar/svg-edit

commit 64812a35fbe56fd40c93fe0245bec5c5ae8fce32

Make foreignObject with foreign content movable, resizable, rotatable
git-svn-id: https://svg-edit.googlecode.com/svn/trunk@1368 eee81c28-f429-11dd-99c0-75d572ba1ddd

editor/svgcanvas.js
@@ -87,6 +87,8 @@ function SvgCanvas(container)
 var isOpera = !!window.opera,
 	isWebkit = navigator.userAgent.indexOf("AppleWebKit") != -1,
 	support = {},
+	htmlns = "http://www.w3.org/1999/xhtml",
+	mathns = "http://www.w3.org/1998/Math/MathML",
 
 // this defines which elements and attributes that we support
 	svgWhiteList = {
@@ -2718,6 +2720,13 @@ function BatchCommand(text) {
 			if (mouse_target.correspondingUseElement)
 				mouse_target = mouse_target.correspondingUseElement;
 	
+			// for foreign content, go up until we find the foreignObject
+			if ($.inArray(mouse_target.namespaceURI, [mathns, htmlns]) != -1) {
+				while (mouse_target.nodeName != "foreignObject") {
+					mouse_target = mouse_target.parentNode;
+				}
+			}
+			
 			// go up until we hit a child of a layer
 			while (mouse_target.parentNode.parentNode.tagName == "g") {
 				mouse_target = mouse_target.parentNode;
@@ -6492,6 +6501,10 @@ function BatchCommand(text) {
 		} else if(elem.nodeName == 'path' && isWebkit) {
 			ret = getPathBBox(selected);
 		} else if(elem.nodeName == 'use' && !isWebkit) {
+			ret = selected.getBBox();
+			ret.x += parseFloat(selected.getAttribute('x'));
+			ret.y += parseFloat(selected.getAttribute('y'));
+		} else if(elem.nodeName == 'foreignObject') {
 			ret = selected.getBBox();
 			ret.x += parseFloat(selected.getAttribute('x'));
 			ret.y += parseFloat(selected.getAttribute('y'));

test/test1.html
@@ -45,10 +45,11 @@
 		// TODO: Test these paths:
 		// "m400.00491,625.01379a1.78688,1.78688 0 1 1-3.57373,0a1.78688,1.78688 0 1 13.57373,0z"
 		// "m36.812,15.8566c-28.03099,0 -26.28099,12.15601 -26.28099,12.15601l0.03099,12.59399h26.75v3.781h-37.37399c0,0 -17.938,-2.034 -133.00001,26.25c115.06201,28.284 130.71801,27.281 130.71801,27.281h9.34399v-13.125c0,0 -0.504,-15.656 15.40601,-15.656h26.532c0,0 14.90599,0.241 14.90599,-14.406v-24.219c0,0 2.263,-14.65601 -27.032,-14.65601zm-14.75,8.4684c2.662,0 4.813,2.151 4.813,4.813c0,2.661 -2.151,4.812 -4.813,4.812c-2.661,0 -4.812,-2.151 -4.812,-4.812c0,-2.662 2.151,-4.813 4.812,-4.813z"
+		// "m 0,0 l 200,0 l 0,100 L 0,100"
 	
 		svgCanvas.setSvgString("<svg xmlns='http://www.w3.org/2000/svg' width='400' x='300'>" +
 								  "<path id='p1' d='M100,100 L200,100 Z'/>" +
-								  "<path id='p2' d='M100,100 L200,100 Z'/>" +
+								  "<path id='p2' d='m 0,0 l 200,0 l 0,100 L 0,100'/>" +
 								"</svg>");
 		
 		var p1 = document.getElementById("p1"),
@@ -72,6 +73,12 @@
 		// convert and verify segments
 		var d = convert(p1, true);
 		equals(d, "m100,100l100,0z", "Converted path to relative string");
+		
+		// TODO: see why this isn't working in SVG-edit
+		d = convert(p2, true);
+		QUnit.log(d);
+		d = convert(p2, false);
+		QUnit.log(d);
 	});
 	
 	module("Transform Module");

commit 0468c20f6a6c81a987a4fd121f3041ee5e874afc

Fix Issue 473: WebKit browsers fixed for navigation, caused by r1368
git-svn-id: https://svg-edit.googlecode.com/svn/trunk@1376 eee81c28-f429-11dd-99c0-75d572ba1ddd

editor/svgcanvas.js
@@ -1992,20 +1992,22 @@ function BatchCommand(text) {
 				m = svgroot.createSVGMatrix();
 			
 			// temporarily strip off the rotate and save the old center
-			var gangle = canvas.getRotationAngle(selected);
-			if (gangle) {
-				for (var i = 0; i < tlist.numberOfItems; ++i) {
-					var xform = tlist.getItem(i);
-					if (xform.type == 4) {
-						// extract old center through mystical arts
-						var rm = xform.matrix;
-						var a = gangle * Math.PI / 180;
-						// FIXME: This blows up if the angle is exactly 0 or 180 degrees!
-						oldcenter.y = 0.5 * (Math.sin(a)*rm.e + (1-Math.cos(a))*rm.f) / (1 - Math.cos(a));
-						oldcenter.x = ((1 - Math.cos(a)) * oldcenter.y - rm.f) / Math.sin(a);
-						tlist.removeItem(i);
-						break;
-					}
+			var a = gangle * Math.PI / 180;
+			if ( Math.abs(a) > (1.0e-10) ) {
+				var s = Math.sin(a)/(1 - Math.cos(a));
+			} else {
+				// FIXME: This blows up if the angle is exactly 0!
+				var s = 2/a;
+			}
+			for (var i = 0; i < tlist.numberOfItems; ++i) {
+				var xform = tlist.getItem(i);
+				if (xform.type == 4) {
+					// extract old center through mystical arts
+					var rm = xform.matrix;
+					oldcenter.y = (s*rm.e + rm.f)/2;
+					oldcenter.x = (rm.e - s*rm.f)/2;
+					tlist.removeItem(i);
+					break;
 				}
 			}
 			
@@ -2223,19 +2225,23 @@ function BatchCommand(text) {
 				m = svgroot.createSVGMatrix(),
 				// temporarily strip off the rotate and save the old center
 				angle = canvas.getRotationAngle(selected);
-			if (angle) {
-				for (var i = 0; i < tlist.numberOfItems; ++i) {
-					var xform = tlist.getItem(i);
-					if (xform.type == 4) {
-						// extract old center through mystical arts
-						var rm = xform.matrix;
-						var a = angle * Math.PI / 180;
-						// FIXME: This blows up if the angle is exactly 0 or 180 degrees!
-						oldcenter.y = 0.5 * (Math.sin(a)*rm.e + (1-Math.cos(a))*rm.f) / (1 - Math.cos(a));
-						oldcenter.x = ((1 - Math.cos(a)) * oldcenter.y - rm.f) / Math.sin(a);
-						tlist.removeItem(i);
-						break;
-					}
+
+			var a = angle * Math.PI / 180;
+			if ( Math.abs(a) > (1.0e-10) ) {
+				var s = Math.sin(a)/(1 - Math.cos(a));
+			} else {
+				// FIXME: This blows up if the angle is exactly 0!
+				var s = 2/a;
+			}
+			for (var i = 0; i < tlist.numberOfItems; ++i) {
+				var xform = tlist.getItem(i);
+				if (xform.type == 4) {
+					// extract old center through mystical arts
+					var rm = xform.matrix;
+					oldcenter.y = (s*rm.e + rm.f)/2;
+					oldcenter.x = (rm.e - s*rm.f)/2;
+					tlist.removeItem(i);
+					break;
 				}
 			}
 			
@@ -2730,7 +2736,11 @@ function BatchCommand(text) {
 				mouse_target = mouse_target.correspondingUseElement;
 	
 			// for foreign content, go up until we find the foreignObject
-			if ($.inArray(mouse_target.namespaceURI, [mathns, htmlns]) != -1) {
+			// WebKit browsers set the mouse target to the svgcanvas div 
+			if ($.inArray(mouse_target.namespaceURI, [mathns, htmlns]) != -1 && 
+				mouse_target.id != "svgcanvas") 
+			{
+				console.log(mouse_target.id);
 				while (mouse_target.nodeName != "foreignObject") {
 					mouse_target = mouse_target.parentNode;
 				}